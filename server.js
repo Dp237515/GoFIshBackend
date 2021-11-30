f8e9b3c7fc9bc8fd9c7bbbd052050d30ee92788f
Dp237515 <71341406+Dp237515@users.noreply.github.com>
2021-11-30 01:21:18 -0500

    Fixed dot env

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const app = express()
-require('dotenv').config();
+require('dotenv').config(); 
 
 //importing all of the controllers
 const cardRoutes = require("./controllers/cardController");
