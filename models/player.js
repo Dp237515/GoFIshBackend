a2466fb3607886b2cc8922fb5e4148f1d9d58f08
Dp237515 <71341406+Dp237515@users.noreply.github.com>
2021-11-30 17:18:09 -0500

    player.js

--- models/player.js
+++ models/player.js
@@ -10,6 +10,7 @@ var playerSchema = new mongoose.Schema({
 	action: { type: String, default: "Waiting", required: false },
 	//how many cards the player has in their hand
 	cardNum: { type: Number, default: 0, required: false },
+	won: { type: Number, default: 0, required: false },
 });
 
 module.exports = new mongoose.model('Player', playerSchema);
